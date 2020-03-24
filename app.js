ec84b89d6f05fe59a50f0ff1c64f982b547bc71a
brunoannunciato <brunobannunciato@gmail.com>
2020-03-24 14:53:21 -0300

    chore and finish

---
+++ app.js
@@ -0,0 +1,24 @@
+let runned = false
+
+const disableEstadao = () => {
+	const paywall = document.querySelector('#paywall-iframe-estadao')
+	const html = document.querySelectorAll('html')[0]
+
+	if (paywall) {
+		paywall.style.display = "none"
+		html.style.overflow = "initial"
+		html.style.top = "0"
+		html.style.position = "initial"
+
+		runned = true
+		!runned && clearInterval(runFunction)
+
+		return
+	}
+	
+	console.log('paywall not found')
+}
+
+if (window.location.href.indexOf('estadao') > 0) {
+	const runFunction = setInterval(disableEstadao, 100)
+}
